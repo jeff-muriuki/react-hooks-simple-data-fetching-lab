b34f322417d79f787be4a2a10a47fba6b482b235
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-04-27 14:42:35 +0300

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+export default function App() {
+    const [randomDog, setRandomDog] = useState(null)
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((r) => r.json())
+        .then((data)=> {
+            setRandomDog(data.message)
+        })
+    },[])
+  return (
+    <div>
+        <p>Here's a Mutina</p>
+       <img src= {randomDog} alt='Random dog'/>
+    </div>
+  )
+}
